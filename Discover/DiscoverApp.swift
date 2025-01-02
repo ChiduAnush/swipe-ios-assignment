4119338ca8b97f2804d6452db0bb575cbd5eb01f
chiduanush <chiduanush1@gmail.com>
2025-01-02 12:26:00 +0530

    Initial Commit

---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by ChiduAnush on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
